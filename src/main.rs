b4368fcacd967cf2efc7fc8c2bc4666241f639bc
Lenny Händler <lenny@lennyhaendler.com>
2019-06-14 03:23:44 +0200

    optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,43 @@
 fn main() {
     let mut max_len = 0;
-    let mut max_sequence: Vec<u64> = vec![];
-    let range = 1..1000000;
-    let mut v = vec![];
+    let max = 1000000;
+    let range = 2..max as u64;
+    let mut cache = vec![1,1];
+    cache.reserve(max);
+    let mut val_for_max = 0;
     for i in range {
-        v = collatz(v, i);
-        if v.len() > max_len {
-            max_len = v.len();
-            max_sequence = v.clone();
+        let l = collatz_size(i, &mut cache);
+        if l > max_len {
+            max_len = l;
+            val_for_max = i;
         }
     }
-    // println!("{:?}", max_sequence);
-    println!("{:?}\nCollatz sequence length: {}", max_sequence, max_len)
+    collatz_series(val_for_max, max_len);
+    println!("collatz sequence length: {}", max_len);
 }
-fn collatz(mut v: Vec<u64>, n: u64) -> Vec<u64> {
-    v.clear();
+fn collatz_series(n: u64, max_len: usize) {
+    let mut v = Vec::with_capacity(max_len);
     let mut i = n;
     while i != 1 {
-        if i % 2 == 0 {
-            i /= 2;
-        } else {
-            i = 3 * i + 1;
-        }
         v.push(i);
+        i = match i {
+            i if i % 2 == 0 => i/2,
+            i => i * 3 + 1
+        };
     }
-    v
+    println!("{:?}", v)
+}
+fn collatz_size(n: u64, cache: &mut Vec<usize>) -> usize {
+    let mut i = n;
+    let mut count = 0;
+    while i >= n {
+        i = match i {
+            i if i % 2 == 0 => i/2,
+            i => i * 3 + 1
+        };
+        count += 1;
+    }
+    let r = cache[i as usize] + count;
+    cache.insert(n as usize, r);
+    r
 }
