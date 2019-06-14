64522873c631541e58b962ea6065a9cd811c8059
Lenny Händler <lenny@lennyhaendler.com>
2019-06-14 17:55:48 +0200

    threading with cache for low numbers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
 extern crate rayon;
 use rayon::prelude::*;
 
+struct Cache {
+    mem: Vec<u16>,
+    limit: usize
+}
+impl Cache {
+    fn new(size: usize) -> Cache {
+        let mut cache = vec![1, 1];
+        cache.reserve(size);
+        let cache = Cache {
+                mem: cache,
+                limit: size
+            };
+        cache
+    }
+}
+
+const CACHE_LIM: usize = 256 * 1024 * 1024;
 fn main() {
-    let max = 200_000_000;
-    let mut cache = vec![1, 1];
-    cache.reserve(max);
+    let max = 2000_000_000u64;
+    
+    use std::cmp;
+    let cache_size = cmp::min(CACHE_LIM, max as usize);
+    let mut cache = Cache::new(cache_size);
+    for x in 2..cache_size as u64 {
+        collatz_size(&x, &mut cache);
+    };
+
     let val_for_max = (2..max)
         .into_par_iter()
-        .max_by_key(|x| collatz_thr(x))//(x, &mut cache))
+        .max_by_key(|x| collatz_thr(x, &cache))
         .unwrap() as u64;
     collatz_series(val_for_max);
 }
@@ -24,19 +47,19 @@ fn collatz_series(n: u64) {
     }
     println!(", length: {}", v)
 }
-fn collatz_thr(n: &usize) -> u32 {
+fn collatz_thr(n: &u64, cache: &Cache) -> u16 {
     let mut i = *n;
     let mut count = 0;
-    while i != 1 {
+    while i as usize >= cache.limit {
         i = match i {
             i if i % 2 == 0 => i / 2,
             i => i * 3 + 1,
         };
         count += 1;
     }
-    count
+    count + cache.mem[i as usize]
 }
-fn collatz_size(n: &usize, cache: &mut Vec<u16>) -> u16 {
+fn collatz_size(n: &u64, cache: &mut Cache) -> u16 {
     let mut i = *n;
     let mut count = 0;
     while i >= *n {
@@ -46,7 +69,7 @@ fn collatz_size(n: &usize, cache: &mut Vec<u16>) -> u16 {
         };
         count += 1;
     }
-    let r = cache[i as usize] + count;
-    cache.insert(*n as usize, r);
+    let r = cache.mem[i as usize] + count;
+    cache.mem.insert(*n as usize, r);
     r
 }
