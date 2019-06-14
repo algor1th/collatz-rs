854fde04daf852fcdfccaa5746f0685dc7c5fc05
Lenny Händler <lenny@lennyhaendler.com>
2019-06-14 15:43:50 +0200

    threading

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,52 @@
+extern crate rayon;
+use rayon::prelude::*;
+
 fn main() {
-    let mut max_len = 0;
-    let max = 1000000;
-    let range = 2..max as u64;
-    let mut cache = vec![1,1];
+    let max = 200_000_000;
+    let mut cache = vec![1, 1];
     cache.reserve(max);
-    let mut val_for_max = 0;
-    for i in range {
-        let l = collatz_size(i, &mut cache);
-        if l > max_len {
-            max_len = l;
-            val_for_max = i;
-        }
-    }
-    collatz_series(val_for_max, max_len);
-    println!("collatz sequence length: {}", max_len);
+    let val_for_max = (2..max)
+        .into_par_iter()
+        .max_by_key(|x| collatz_thr(x))//(x, &mut cache))
+        .unwrap() as u64;
+    collatz_series(val_for_max);
 }
-fn collatz_series(n: u64, max_len: usize) {
-    let mut v = Vec::with_capacity(max_len);
+fn collatz_series(n: u64) {
+    let mut v = 0;
     let mut i = n;
     while i != 1 {
-        v.push(i);
+        print!("{} ", i);
+        v += 1;
         i = match i {
-            i if i % 2 == 0 => i/2,
-            i => i * 3 + 1
+            i if i % 2 == 0 => i / 2,
+            i => i * 3 + 1,
         };
     }
-    println!("{:?}", v)
+    println!(", length: {}", v)
 }
-fn collatz_size(n: u64, cache: &mut Vec<usize>) -> usize {
-    let mut i = n;
+fn collatz_thr(n: &usize) -> u32 {
+    let mut i = *n;
     let mut count = 0;
-    while i >= n {
+    while i != 1 {
         i = match i {
-            i if i % 2 == 0 => i/2,
-            i => i * 3 + 1
+            i if i % 2 == 0 => i / 2,
+            i => i * 3 + 1,
+        };
+        count += 1;
+    }
+    count
+}
+fn collatz_size(n: &usize, cache: &mut Vec<u16>) -> u16 {
+    let mut i = *n;
+    let mut count = 0;
+    while i >= *n {
+        i = match i {
+            i if i % 2 == 0 => i / 2,
+            i => i * 3 + 1,
         };
         count += 1;
     }
     let r = cache[i as usize] + count;
-    cache.insert(n as usize, r);
+    cache.insert(*n as usize, r);
     r
 }
